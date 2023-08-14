a0aeaf8cbff3cbcb0b6c841ff004d9076d8030a8
Mish-Al <mtaliep.lifechoices@gmail.com>
2023-08-14 14:40:52 +0200

    controllerUsers

--- api/model/index.js
+++ api/model/index.js
@@ -1,1 +1,8 @@
-// model
+const Users = require('./Users')
+const Orders = require('./Orders')
+const Books = require('./Books')
+const BookAuthors = require('./BookAuthors')
+//export all objects
+module.exports = {
+    users: new Users(),
+}
