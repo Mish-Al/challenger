8ae61fc580511cc84e7193d119683b324649d559
Mish-Al <mtaliep.lifechoices@gmail.com>
2023-08-17 14:41:41 +0200

    Updates

--- api/controller/index.js
+++ api/controller/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const {verifyAToken} = require('../middleware/AuthenticateUser')
 const routes = express.Router();
 //import all model's objects
-const { users } = require("../model");
+const {users, books, authors, orders } = require("../model");
 
 //Users router
 routes.get("/users", (req, res) => {
@@ -11,7 +12,7 @@ routes.get("/users", (req, res) => {
 routes.get("/user/:id", (req, res) => {
   users.fetchUser(req, res);
 });
-routes.post("register", bodyParser.json(), (req, res) => {
+routes.post("/register", bodyParser.json(), (req, res) => {
   users.register(req, res);
 });
 routes.put("/user/:id", bodyParser.json(), (req, res) => {
@@ -23,10 +24,17 @@ routes.patch("/user/:id", bodyParser.json(), (req, res) => {
 routes.delete("/user/:id", (req, res) => {
   users.deleteUser(req, res);
 });
+routes.post('/login',
+bodyParser.json(), (req, res)=>{
+    users.login(req, res)
+});
+// book's router
+routes.get('/books', verifyAToken,)
 
 module.exports = {
     express,
-    routes
+    routes,
+    verifyAToken
 }
 
 
--- api/model/Orders.js
+++ api/model/Orders.js
@@ -1,4 +1,20 @@
-class Orders{
+const db = require('../config')
 
+class Orders{
+    fetchOrders(req, res) {
+
+    }
+    fetchOrder(req, res) {
+
+    }
+    addOrder(req, res) {
+
+    }
+    updateOrder(req, res) {
+
+    }
+    deleteOrder(req, res) {
+
+    }
 }
 module.exports = Orders
